1da401e47d1250f793a1634fb149987de6869734
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-06-10 20:27:47 +0700

    update: adding zod and react-hook-form

---
+++ src/components/form/AddPropertyForm.tsx
@@ -0,0 +1,8 @@
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export const AddPropertyForm = ({}) => {
+  return <></>;
+};
