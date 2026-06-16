cf83a482f0f578f85106dcc37efbc98a46dc6663
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-06-16 21:04:56 +0700

    update: add listing page and api implementation

--- src/components/form/AddPropertyPage.tsx
+++ src/components/form/AddPropertyPage.tsx
@@ -1,8 +1,29 @@
 import { AddPropertyForm } from "@/components/form/AddPropertyForm";
+import { toast } from "sonner";
+import { addListing } from "@/lib/api/listings";
 
-export const AddPropertyPage = ({ apiUrl }: { apiUrl: string }) => {
+export const AddPropertyPage = ({
+  apiUrl,
+  token,
+}: {
+  apiUrl: string;
+  token?: string;
+}) => {
   const handleSubmit = async (values: any) => {
-    console.log(values);
+    const { error, status } = await addListing({
+      apiUrl,
+      data: values,
+      token,
+    });
+
+    if (error) {
+      if (status === 401) {
+        window.location.href = "/login";
+      }
+      toast.error(`Error adding property (status ${status})`);
+    } else {
+      toast.success("Property successfully added");
+    }
   };
 
   return (
